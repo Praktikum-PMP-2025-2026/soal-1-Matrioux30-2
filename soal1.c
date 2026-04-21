a17fda1e2604507e5a6548f66e7b1e2aec7eea04
Matrioux30 <izhharulmulki@gmail.com>
2026-04-21 10:58:33 +0700

    Refactor main function to use fgets and fix print.
    .

--- soal1.c
+++ soal1.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) {
-    char naskah[100];
-    scanf("%s", &naskah);
-    printF("%s", naskah);
-    return 0;
+int main(){
+    char naskah[30];
+    int array[30];
+    fgets(naskah, sizeof(naskah), stdin);
+    printf("%s", naskah);
 }
